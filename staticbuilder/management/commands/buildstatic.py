480e896d1a3f93ebb865ce0eb52b800520adfb93
Chris McKenzie <chrismc@hzdg.com>
2013-02-04 12:31:44 -0500

    logging stderrs instead of raising Exception

--- staticbuilder/management/commands/buildstatic.py
+++ staticbuilder/management/commands/buildstatic.py
@@ -89,7 +89,7 @@ class Command(BaseCommand):
             self.log(stdout, level=2)
 
         if stderr:
-            raise Exception(stderr)
+            self.log(t.bold('Stderr:\n') + stderr)
 
     def collect_for_build(self, build_dir):
         with buildable_files_finders():
